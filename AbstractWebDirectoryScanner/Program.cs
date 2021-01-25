56cff732a4ee4a207b892169b9a69f88e6327921
dr.lis <dr.lis@outlook.com>
2021-01-25 17:52:39 +0300

    Arguments parsing completed

--- AbstractWebDirectoryScanner/Program.cs
+++ AbstractWebDirectoryScanner/Program.cs
@@ -4,9 +4,29 @@ namespace AbstractWebDirectoryScanner
 {
 	class Program
 	{
+		static void Scan(String url, String pathToFile)
+		{
+
+		}
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			if (args.Length != 2)
+			{
+				Console.WriteLine("Please, type URL and path to dictionary");
+			}
+			else
+			{
+				try
+				{
+					Console.WriteLine($"{args[0]} {args[1]}");
+					Scan(args[0], args[1]);
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e.Message);
+				}
+			}
 		}
 	}
 }
